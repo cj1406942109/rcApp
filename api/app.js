9b0edbfb3f6a8d8147d0352563dff542404ca565
cj1406942109 <1406942109@qq.com>
2017-05-17 12:06:11 +0800

    设置上传目录为静态资源

--- api/app.js
+++ api/app.js
@@ -27,7 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('uploads',express.static(path.join(__dirname, 'uploads')));
 
 app.use(session({
     secret: config.cookieSecret,
